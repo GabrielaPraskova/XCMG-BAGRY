ec49bbec6766c2276b403413fdf847992eeaa6df
MartinaSulova <64073790+MartinaSulova@users.noreply.github.com>
2020-06-17 18:06:50 +0200

    pujcovna

--- src/views/Pujcovna.vue
+++ src/views/Pujcovna.vue
@@ -2,12 +2,12 @@
   <div>
     
     <router-link to="/stroje">STROJE</router-link>|
-    <router-link to="/pujcovna">PUJCOVNA</router-link>|
+    <router-link to="/pujcovna">PŮJCOVNA</router-link>|
     <router-link to="/servis">SERVIS</router-link>|
     <router-link to="/spolecnost">O SPOLEČNOSTI</router-link>|
     <router-link to="/kontakt">KONTAKT</router-link>
 
-    <h1>PUJCOVNA</h1>
+    <h1>PŮJCOVNA</h1>
   </div>
 </template>
 
