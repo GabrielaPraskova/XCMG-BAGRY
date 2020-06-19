b05fdf6d89a8efb0ea77b2bfc6f69e33b8d48ce6
MartinaSulova <64073790+MartinaSulova@users.noreply.github.com>
2020-06-19 12:23:19 +0200

    servis

--- src/views/Servis.vue
+++ src/views/Servis.vue
@@ -4,6 +4,24 @@
   
 
     <h1>SERVIS</h1>
+    <h3>  Záruční a pozáruční servis a prodej náhradních dílů pro stroje SANY v České Republice zajišťuje: </h3>
+    ​<p>CARACAL MACHINES s. r. o. – Králův Dvůr u Berouna – prodej strojů, příslušenství a náhradních dílů, servis, půjčovna, lokality: Beroun, České Budějovice, Liberec, Bruntál, Lovosice
+    Rocktech s.r.o. – Králův Dvůr u Berouna – prodej strojů a příslušenství, servis strojů a příslušenství
+    VZ service - Vladimír Zelenka - České Budějovice - prodej strojů a příslušenství, servis strojů a tiltrotátorů engcon</p>
+    <h3>Pro servis strojů SANY, ale i CAT, Volvo, JCB, Bobcat, Komatsu volejte prosím: </h3>
+
+    <p>Telefon: +420 604 489 121
+
+     <br/>E-Mail: servis@caracal-machines.cz</p>
+
+    
+
+    <h3>Náhradní díly pro stroje SANY, CAT, Volvo, Komatsu, Bobcat, JCB objednávejte:</h3>
+
+    <p >Telefon: +420 604 446 813 
+
+    <br/>E-Mail: dily@caracal-machines.cz   </p>
+    <p></p>
 
   </div>
 </template>
