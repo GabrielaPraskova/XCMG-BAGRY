711dbe58e96df95c7811ad861ba0e181243fed18
GabrielaPraskova <gab.praskova@gmail.com>
2020-06-21 13:47:05 +0200

    paticka

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,111 @@
-
 <template>
-<div id="app">
-  <head>
-    <link
-      href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap"
-      rel="stylesheet"
-    />
-  </head>
+  <div id="app">
+    <head>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap"
+        rel="stylesheet"
+      />
+    </head>
 
-  <div id="nav">
-    <div class="prvniMenu">
-      <div class="kontakt">
-        <div class="telefon">
-          <v-icon color="#FFEB3B">mdi-phone</v-icon>+420 733 798 032
-        </div>
-        <div class="email">
-          <v-icon color="#FFEB3B">mdi-email</v-icon>xcmg@servis.com
-        </div>
-      </div>
-
-      <div class="servis">
-        <div class="servisniHodiny">Servisní hodiny (8:00 - 17:00)</div>
-      </div>
-    </div>
-
-    <div class="druheMenu">
-      <div class="logo">
-        <router-link to="/">
-          <img src="/logo.png" alt="XCMG" />
-        </router-link>
-      </div>
-
-      <ul class="menu">
-        <div class="menuLink" >
-          Stroje
-          <div class="podmenu">
-            <router-link class="menuLink" to="/Konfigurator2/bagr">Kolové bagry</router-link>
-            <router-link class="menuLink" to="/Konfigurator2/rypadlo">Rypadla</router-link>
+    <div id="nav">
+      <div class="prvniMenu">
+        <div class="kontakt">
+          <div class="telefon">
+            <v-icon color="#FFEB3B">mdi-phone</v-icon>+420 733 798 032
+          </div>
+          <div class="email">
+            <v-icon color="#FFEB3B">mdi-email</v-icon>xcmg@servis.com
           </div>
         </div>
-        <router-link class="menuLink" to="/pujcovna">Půjčovna</router-link>
-        <router-link class="menuLink" to="/servis">Servis</router-link>
-        <router-link class="menuLink" to="/spolecnost">O společnosti</router-link>
-        <router-link class="menuLink" to="/kontakt">Kontakt</router-link>
-      </ul>
+
+        <div class="servis">
+          <div class="servisniHodiny">Servisní hodiny (8:00 - 17:00)</div>
+        </div>
+      </div>
+
+      <div class="druheMenu">
+        <div class="logo">
+          <router-link to="/">
+            <img src="/logo.png" alt="XCMG" />
+          </router-link>
+        </div>
+
+        <ul class="menu">
+          <div class="menuLink">
+            Stroje
+            <div class="podmenu">
+              <router-link class="menuLink" to="/Konfigurator2/bagr"
+                >Kolové bagry</router-link
+              >
+              <router-link class="menuLink" to="/Konfigurator2/rypadlo"
+                >Rypadla</router-link
+              >
+            </div>
+          </div>
+          <router-link class="menuLink" to="/pujcovna">Půjčovna</router-link>
+          <router-link class="menuLink" to="/servis">Servis</router-link>
+          <router-link class="menuLink" to="/spolecnost"
+            >O společnosti</router-link
+          >
+          <router-link class="menuLink" to="/kontakt">Kontakt</router-link>
+        </ul>
+      </div>
+    </div>
+
+    <div class="router">
+      <router-view />
+    </div>
+    <footer>
+      <div class="paticka">
+        <ul class="kont">
+          <li class="k">Kontakty</li>
+          <li>Dolní 141</li>
+          <li>582 91 Světlá nad Sázavou</li>
+          <li>733 798 032</li>
+          <li>xcmg@servis.com</li>
+        </ul>
+
+        <ul class="servis">
+          <li class="s">Servisní hodiny</li>
+          <li>Po - Pá: 8 - 17</li>
+          <li>nebo dle dohody</li>
+        </ul>
+
+        <ul class="ikonky">
+          <li class="i"></li>
+          <li class="facebook">
+            <a href="https://www.facebook.com/zetor" target="_blank"
+              ><img class="obrazekFb" src="/fb.png"
+            /></a>
+          </li>
+          <li class="linked">
+            <a
+              href="https://www.linkedin.com/company/zetor-tractors-a-s-?trk=nav_account_sub_nav_company_admin"
+              target="_blank"
+              ><span class="icon icon-linkedin"></span
+            ></a>
+          </li>
+          <li class="youtube">
+            <a
+              href="https://www.youtube.com/channel/UCMNdnC74YttvjNuTzkYoigQ"
+              target="_blank"
+              ><span class="icon icon-youtube"></span
+            ></a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+
+    <div class="copy">
+      copyright &copy; {{ new Date().getFullYear() }} XCMG/GM
     </div>
   </div>
-
-  <router-view />
-</div>
 </template>
 
-
-
 <style scoped>
+.router{
+min-height: calc(100vh - 307px);
+}
 .prvniMenu {
   display: flex;
   justify-content: space-between;
@@ -79,7 +131,7 @@
 }
 
 .kontakt {
-  padding: 10px
+  padding: 10px;
 }
 
 .menu {
@@ -103,8 +155,7 @@
 
 .menuLink:hover {
   background-color: white;
-  color: #16405C;
-
+  color: #16405c;
 }
 
 #app {
@@ -113,6 +164,7 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  
 }
 
 /* #nav {
@@ -125,11 +177,11 @@
 }
 
 #nav a:hover {
-  color: #16405C
+  color: #16405c;
 }
 
 #nav a.router-link-exact-active {
-  color: #16405C;
+  color: #16405c;
 }
 
 h1,
@@ -159,5 +211,21 @@ h3 {
 
 .telefon {
   padding-right: 20px;
+}
+
+.paticka {
+  display: flex;
+  list-style: none;
+  background-color: #eeeeee;
+}
+
+.copy {
+  background-color: #3498db;
+  padding: 10px;
+}
+
+.obrazekFb {
+  max-width: 5%;
+  border-radius: 50%;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,34 +6,7 @@
     <router-link  to="/Konfigurator1"><v-btn class="ma-2" outlined color="#3498db">Konfiguruj</v-btn></router-link>
     <v-btn class="ma-2" outlined color="#3498db"><router-link  to="/Konfigurator2">Konfiguruj</router-link></v-btn>
 
-    <footer>
-      <div class="paticka">
-        <ul class="kont">
-          <li class="k">Kontakty</li>
-          <li>Dolní 141</li>
-          <li>582 91 Světlá nad Sázavou</li>
-          <li>733 798 032</li>
-          <li>xcmg@servis.com</li>
-        </ul>
-
-        <ul class="servis">
-          <li class="s">Servisní hodiny</li>
-          <li>Po - Pá: 8 - 17</li>
-          <li>nebo dle dohody</li>
-        </ul>
-
-        <ul class="ikonky">
-          <li class="i">
-          <li class="facebook"><a href="https://www.facebook.com/zetor" target="_blank"><img class="obrazekFb" src="/fb.png"></a></li>
-          <li class="linked"><a href="https://www.linkedin.com/company/zetor-tractors-a-s-?trk=nav_account_sub_nav_company_admin" target="_blank"><span class="icon icon-linkedin"></span></a></li>
-          <li class="youtube"><a href="https://www.youtube.com/channel/UCMNdnC74YttvjNuTzkYoigQ" target="_blank"><span class="icon icon-youtube"></span></a></li>
-        </ul>
-
-      </div>
-      
-    </footer>
-
-    <div class="copy">copyright &copy; {{ new Date().getFullYear() }}  XCMG/GM </div>
+    
   </div>
 </template>
 
@@ -60,12 +33,6 @@ export default {};
     padding: 200px 30px;
 } */
 
-.paticka {
-  display: flex;
-  list-style: none; 
-  background-color: #eeeeee;
-  
-}
 
 .kont,
 .servis {
@@ -79,11 +46,6 @@ export default {};
   margin-right: 50px;
   margin-left: 50px;
   
-}
-
-.copy {
-   background-color: #3498db;
-   padding: 10px;
 }
 
 .k,
@@ -100,8 +62,5 @@ export default {};
  list-style: none;
 }
 
-.obrazekFb {
-  max-width: 5%;
-  border-radius: 50%;
-}
+
 </style>
