1ee164208cad54f692ddb5260a57f849afccc04f
GabrielaPraskova <gab.praskova@gmail.com>
2020-06-21 11:00:25 +0200

    rypadla

--- src/views/Konfigurator2.vue
+++ src/views/Konfigurator2.vue
@@ -1,64 +1,214 @@
+
+
 <template>
   <div>
     <div class="kontejner">
-      <img class="rypadlo" src="/XE250.jpg" alt="obrazekXE250" />
+      <div class="prvni">
+        <img
+          class="celniObr"
+          v-bind:src="(`/rypadla/${aktivniRypadlo.obrazek}`)"
+          alt="obrazekBagrRypadla"
+        />
+        <p>{{aktivniRypadlo.technickeParametry}}</p>
+      </div>
 
-      <div class="prostredek">
-        <h1>ZL50G(CE)</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae fugiat, alias libero quas quibusdam nisi? Labore magnam eum, exercitationem adipisci dolore velit architecto quae ipsa facilis aspernatur hic, odit aperiam!</p>
-      
-        <v-col class="d-flex" cols="12" sm="6">
-          <v-select :items="items" label="Nosnost" dense outlined></v-select>
-        </v-col>
+      <div class="tlacitkaKonfigurator">
+        <div v-if="aktivniStranka === 0" class="druhy">
+          <h1>ZL50G(CE)</h1>
+          <button
+            v-on:click="nastavAktivniRypadlo(rypadlo)"
+            v-bind:class="{active: rypadlo === aktivniRypadlo}"
+            v-for="(rypadlo,index) in rypadla"
+            v-bind:key="index"
+          >
+            <div>Hmotnost:{{rypadlo.technickeParametry.hmotnost}}t</div>
+          </button>
+
           <v-col cols="12" sm="6" md="4">
-          <v-subheader>Motor</v-subheader>
-          <v-radio-group v-model="direction" hide-details>
-            <v-radio value="top" label="MT30"></v-radio>
-            <v-radio value="right" label="XD50"></v-radio>
-          
-          </v-radio-group>
-        </v-col>
-        <v-col class="d-flex" cols="12" sm="6">
-          <v-select :items="items" filled label="Interiér" dense></v-select>
-        </v-col>
+            <v-subheader>{{aktivniMotor}}</v-subheader>
+            <v-radio-group v-model="aktivniMotor" hide-details>
+              <v-radio
+                v-for="(motor, index) in aktivniRypadlo.motor"
+                v-bind:key="index"
+                :value="motor.nazevMotoru"
+                :label="motor.nazevMotoru"
+              ></v-radio>
+              <!-- <v-radio value="XD50" label={{motor.nazevMotoru}}></v-radio> -->
+            </v-radio-group>
+          </v-col>
+        </div>
 
+        <div class="barvy">
+          <v-subheader>Barva</v-subheader>
+
+          <button
+            v-on:click="vyberBarvu(barva)"
+            v-for="(barva, index) in Data.barvy"
+            v-bind:key="index"
+            v-bind:style="`background: ${barva}`"
+          ></button>
+        </div>
       </div>
 
-      <div class="pravaStrana">
-        <h1>Cena</h1>
-        <p>bagr 890 000,- Kč</p>
-        <p>naklikane polozky</p>
-        <hr />
-        <p>Cena celkem bez DPH</p>
-
-        <p>Běžný čas dodání stroje je 2 měsíce od odeslání poptávky</p>
-
-        <p>Máte-li zájem o konkrétní konfiguraci stroje, napište nám a my se vám ozveme do následujícího pracovního dne</p>
-        <v-btn class="ma-2" outlined color="#3498db">Nezávazná Poptávka</v-btn>
+      <div class="druhy3" v-if="aktivniStranka === 2">
+        <div v-for="(polozka, index) in vyberRypadla" v-bind:key="index">
+          <!-- <img class="prislusenstvi" v-bind:src="(`/rypadla/${polozka.obrazek}`)" alt="lzice" /> -->
+          <v-checkbox v-model="aktivniPrislusenstvi[polozka.id]" :label="`${polozka.nazev}`"></v-checkbox>
+        </div>
       </div>
+      <div>
+        <v-btn
+          v-on:click="prev"
+          class="ma-2"
+          outlined
+          color="#3498db"
+          v-if="aktivniStranka > 0"
+        >PREVIOUS</v-btn>
+
+        <v-btn
+          v-on:click="next"
+          class="ma-2"
+          outlined
+          color="#3498db"
+          v-if="aktivniStranka < 2"
+        >NEXT</v-btn>
+      </div>
+    </div>
+
+    <div class="treti">
+      <h1>Cena</h1>
+      <p>rypadlo 890 000,- Kč</p>
+      <p>naklikane polozky {{aktivniBarva}}, {{aktivniMotor}}, {{vyberPrislusenstvi}}</p>
+      <hr />
+      <p>Cena celkem bez DPH</p>
+
+      <p>Běžný čas dodání stroje je 2 měsíce od odeslání poptávky</p>
+
+      <p>Máte-li zájem o konkrétní konfiguraci stroje, napište nám a my se vám ozveme do následujícího pracovního dne</p>
+      <v-btn class="ma-2" outlined color="#3498db">Nezávazná Poptávka</v-btn>
     </div>
   </div>
 </template>
 
 <script>
-export default {
+import Data from "./../assets/Data/data.js";
 
-}
+console.log(Data.prislusenstvi);
+let rypadla = Data.stroje.filter(stroj => stroj.typ === "rypadlo");
+
+export default {
+  data() {
+    return {
+      Data,
+      aktivniRypadlo: rypadla[0],
+      rypadla,
+      aktivniStranka: 0,
+      aktivniMotor: rypadla[0].motor[0].nazevMotoru,
+      aktivniBarva: "yellow",
+      aktivniPrislusenstvi: {}
+    };
+  },
+
+  computed: {
+    vyberRypadla() {
+      const rypadla = Data.prislusenstvi.filter(
+        polozka => polozka.typ === "rypadlo"
+      );
+      console.log(rypadla);
+      return rypadla;
+    },
+
+    // vyberNastandart(){
+    //   const nazvyNadstandart = Object.entries(this.aktivniNadstandart)
+    //     .filter(nadstandard => nadstandard[1] === true)
+    //     .map(rypadlo =>
+    //     {
+    //       const id = rypadlo[0];
+    //       const nadstandardnaPolozka = Data.nadstandart.find( polozka => polozka.id.toString() === id.toString() );
+    //       return nadstandardnaPolozka.nazev
+    //     })
+
+    //   return nazvyNadstandart
+    // },
+
+    vyberPrislusenstvi() {
+      const nazvyPrislusenstvi = Object.entries(this.aktivniPrislusenstvi)
+        .filter(prislusenstvi => prislusenstvi[1] === true)
+        .map(smetak => {
+          const id = smetak[0];
+          const nadstandardnaPolozka = Data.prislusenstvi.find(
+            polozka => polozka.id.toString() === id.toString()
+          );
+          return nadstandardnaPolozka.nazev;
+        });
+
+      return nazvyPrislusenstvi;
+    }
+  },
+
+  methods: {
+    nastavAktivniRypadlo(rypadlo) {
+      this.aktivniRypadlo = rypadlo;
+      this.aktivniMotor = this.aktivniRypadlo.motor[0].nazevMotoru;
+    },
+
+    next() {
+      this.aktivniStranka++;
+    },
+
+    prev() {
+      this.aktivniStranka--;
+    },
+    vyberBarvu(color) {
+      this.aktivniBarva = color;
+    }
+  }
+};
 </script>
 
 <style>
 .kontejner {
   display: flex;
-  margin: 50px;
 }
 
-.prostredek,
-.pravaStrana {
+.prvni,
+.tlacitkaKonfigurator,
+.treti {
+  flex-basis: 33.3%;
+}
+
+.prvni,
+.druhy {
+  padding-right: 50px;
+}
+.druhy,
+.treti {
   text-align: left;
 }
 
-.rypadlo {
-  max-width: 30%;
-  height: auto;
+.celniObr {
+  max-width: 50%;
+  align-self: center;
+}
+
+.active {
+  border: 2px solid #3498db;
+  padding: 10px;
+  border-radius: 5px;
+  text-align: left;
+  margin: 10px;
+}
+
+button {
+  border: 1px solid #3498db;
+  padding: 10px;
+  border-radius: 5px;
+  text-align: left;
+  margin: 10px;
+}
+
+.prislusenstvi {
+  max-width: 10%;
+  display: block;
 }
 </style>
