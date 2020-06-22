2733907fa599e9c35726d9e86f7fe1ab89e0cb60
MartinaSulova <64073790+MartinaSulova@users.noreply.github.com>
2020-06-22 17:09:16 +0200

    xyz

--- src/views/Kontakt.vue
+++ src/views/Kontakt.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="kontejner">
     <div class="kontakt">
-      <div class="formular">
+      <div class="vedeniSpolecnosti">
         <h1>Kontakt</h1>
 
           <h3>Adresa</h3>
@@ -11,52 +11,40 @@
               <br />IČ: 08757127 | DIČ: CZ08757127
               <br />info@pkbau.cz
             </p>
-          <hr />
+          
           <br />
           <h3>Vedení společnosti</h3>
-            <p>
-                <span>Ing. Martin Prášek – jednatel
+             <div>
+               <p>
+               Ing. Martin Prášek – jednatel
                 <br />tel: +420 777 728 275
-                <br />e-mail: prasek@xcmg.cz</span>
-                <span class="druhyspan"> <br />Ing. Radek Klofáč – jednatel
+                <br />e-mail: prasek@xcmg.cz <p/></div>
+                <div class="druhyspan"> 
+                  <p><br />Ing. Radek Klofáč – jednatel
                 <br />tel: +420 777 728 063
-                <br />e-mail: klofac@xcmg.cz</span>
-            </p>
-        <hr />
-        <br />
-          <h3>Kancelář</h3>
-        <!-- <p>
-          Ivana Jarošová – asistentka
-          <br />tel: +420 601 300 140
-          <br />e-mail: xcmg@xcmg.cz
-        </p>
-        <hr />
-        <br />-->
+                <br />e-mail: klofac@xcmg.cz
+              </p>
+            </div>
+      </div>
+      <div class="kancelar">
+        <h3>Kancelář</h3>
+       
         <h3>Otevírací doba:</h3>
           <p>
             Po – Pá 8:00 – 17:30
             <br />nebo dle telefonické dohody
           </p>
-        <div class="napisteNam"> 
+      </div>
+
+
+      <div class="napisteNam"> 
           <h3>Napište nám</h3>
-          <br />
 
-            <div class="jmenoPrijmeni">
-              <v-col cols="12" sm="6" md="10">
-                <v-text-field label="Jméno a příjmení" outlined color="#3498db"></v-text-field>
-              </v-col>
-              <!-- <v-col cols="12" sm="6" md="6">
-                  <v-text-field label="Příjmení" outlined color="#3498db"></v-text-field>
-              </v-col>-->
-            </div>
-            <v-col cols="12" sm="6" md="10">
-              <v-text-field label="Email" outlined color="#3498db"></v-text-field>
-            </v-col>
-
-              <v-textarea outlined color="#3498db" name="input-7-4" label value></v-textarea>
-              <v-btn class="ma-2" outlined color="#3498db">Odeslat</v-btn>
-          </div>
-        </div>
+          <label> Jméno a přijmení
+            <input type="text">
+          </label>
+          
+      </div>
     </div>
     <div class="obrazek">
       <div>
@@ -74,22 +62,26 @@ export default {};
 .kontejner {
   display: flex;
   justify-content: space-around;
+  margin-top: 35px;
 
 }
 .kontakt h3 {
   color: #3498db;
   text-align: left;
-  margin-left: 20px;
+  margin-top: 10;
+  margin-bottom: 20px;
 }
 
 .kontakt h1 {
   text-align: left;
-  margin-left:20px;
+  margin-bottom:20px;
   
 }
+
 .kontakt p {
   text-align: left;
-  padding: 20px;
+  padding: 0px;
+  margin-bottom: 5px;
 }
 
 .jmenoPrijmeni {
@@ -98,5 +90,9 @@ export default {};
 
 .mapa {
   max-width: 100%;
+}
+
+.druhyspan{
+  padding-top: 5px;
 }
 </style>
