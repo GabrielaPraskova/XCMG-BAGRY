08b3d46f1d0b8288a439e396ad99c79e32d55709
MartinaSulova <64073790+MartinaSulova@users.noreply.github.com>
2020-06-23 17:29:26 +0200

    jj

--- src/views/Pujcovna.vue
+++ src/views/Pujcovna.vue
@@ -1,68 +1,68 @@
 <template>
-<div class="pujcovnaKontejner">
-  <div class="pujcovna">
-    <div class="pujcovnaSloupec">
-      <div class="pujcovnaPopis">
-        <h1>PUJČOVNA</h1>
-        <p class="pujcovnaPrvniOdstavec">
-          Každý stavař zná situaci, kdy termíny realizace zakázek tlačí a
-          stávající technika vypoví službu. Ať už jste malý podnikatel s velkými
-          záměry nebo velký podnikatel s malými záměry, anebo úplně jinak,
-          věříme, že jsme schopni pokrýt jakoukoliv vaší poptávku, dlouhodobého
-          i krátkodobého charakteru, na zapůjčení techniky pro realizaci toho,
-          co jste si předsevzali.
-        </p>
+  <div class="pujcovnaKontejner">
+    <div class="pujcovna">
+      <div class="pujcovnaSloupecJedna">
+        <div class="pujcovnaPopis">
+          <h1>PUJČOVNA</h1>
+          <p class="pujcovnaPrvniOdstavec">
+            Každý stavař zná situaci, kdy termíny realizace zakázek tlačí a
+            stávající technika vypoví službu. Ať už jste malý podnikatel s velkými
+            záměry nebo velký podnikatel s malými záměry, anebo úplně jinak,
+            věříme, že jsme schopni pokrýt jakoukoliv vaší poptávku, dlouhodobého
+            i krátkodobého charakteru, na zapůjčení techniky pro realizaci toho,
+            co jste si předsevzali.
+          </p>
 
-        <p class="pujcovnaDruhyOdstavec">
-          Pokud v uvedeném seznamu strojů z půjčovny nenaleznete takový, jaký
-          potřebujete, nezoufejte, sdílíme sklad s Německým partnerem a
-          poptávaný stroj dokážeme dodat během několika dní až k vám na stavbu.
-        </p>
+          <p class="pujcovnaDruhyOdstavec">
+            Pokud v uvedeném seznamu strojů z půjčovny nenaleznete takový, jaký
+            potřebujete, nezoufejte, sdílíme sklad s Německým partnerem a
+            poptávaný stroj dokážeme dodat během několika dní až k vám na stavbu.
+          </p>
 
-        <h3 class="pujcovnaAdresaNadpis">Administrátor půjčovny</h3>
-        <ul class="pujcovnaAdresa">
-          <li>​Martin Prášek</li>
-          <li>Telefon: +420 733 798 032</li>
-          <li>E-Mail: pujcovna@xcmg.cz​​​​</li>
-        </ul>
+          <h3 class="pujcovnaAdresaNadpis">Administrátor půjčovny</h3>
+          <ul class="pujcovnaAdresa">
+            <li>​Martin Prášek</li>
+            <li>Telefon: +420 733 798 032</li>
+            <li>E-Mail: pujcovna@xcmg.cz​​​​</li>
+          </ul>
 
+        </div>
       </div>
-    </div>
 
-    <div class="pujcovnaSloupec">
-    
-      <div class="obrazky">
-        <div class="prvniObrazek">
-          <img class="celniObr" src="/LW800KN.jpg" alt="obrazekLW800KN" />
-          <div>
-            <h3>LW800KN</h3>
-            alsdkfj;asdifj
-            <h3>Cena</h3>
-            od 1500 Kč
+      <div class="pujcovnaSloupecDva">
+      
+        <div class="obrazky">
+          <div class="prvniObrazek">
+            <img class="celniObr" src="/LW800KN.jpg" alt="obrazekLW800KN" />
+            <div>
+              <h3>LW800KN</h3>
+              alsdkfj;asdifj
+              <h4>Cena</h4>
+              od 1500 Kč / den
+            </div>
           </div>
-        </div>
-        <div class="druhyObrazek">
-          <img class="celniObr" src="/ZL50G.jpg" alt="obrazekLW800KN" />
-          <div>
-            <h3>LW800KN</h3>
-            alsdkfj;asdifj
-            <h3>Cena</h3>
-            od 1500 Kč
+          <div class="druhyObrazek">
+            <img class="celniObr" src="/ZL50G.jpg" alt="obrazekLW800KN" />
+            <div>
+              <h3>LW800KN</h3>
+              alsdkfj;asdifj
+              <h4>Cena</h4>
+              od 1500 Kč / den
+            </div>
           </div>
-        </div>
-        <div class="tretiObrazek">
-          <img class="celniObr" src="/bagr/ZL30G.jpg" alt="obrazekLW800KN" />
-          <div>
-            <h3>LW800KN</h3>
-            alsdkfj;asdifj
-            <h3>Cena</h3>
-            od 1500 Kč
+          <div class="tretiObrazek">
+            <img class="celniObr" src="/bagr/ZL30G.jpg" alt="obrazekLW800KN" />
+            <div>
+              <h3>LW800KN</h3>
+              alsdkfj;asdifj
+              <h4>Cena</h4>
+              od 1500 Kč / den
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -86,12 +86,14 @@ export default {};
   margin-bottom: 20px;
   color:#3498db
 }
+
 h3 {
   text-align: left;
 } 
 
 .celniObr {
-  max-width: 30%;
+  max-width: 40%;
+  margin-right: 20px;
 }
 
 .prvniObrazek,
@@ -99,8 +101,6 @@ h3 {
 .tretiObrazek {
   display: flex;
 }
-
-
 
 .pujcovnaAdresa {
   list-style: none;
@@ -118,8 +118,24 @@ h3 {
  
 }
 
-.pujcovnaSloupec{
+.pujcovnaSloupecJedna,
+.pujcovnaSloupecDva{
   flex:1;
+}
+
+.pujcovnaSloupecDva{
+  display: flex;
+  margin-left:50px;
+  margin-top: 30px;
+}
+
+.pujcovnaSloupecDva h3,
+.pujcovnaSloupecDva h4{
+  margin: 0px;
+  color:#3498db;
+  }
+.pujcovnaSloupecDva h3{
+  margin-top: 30px;
 }
 
 
