dcb4dd3097dc2ea183aa2f2e535cae2407b84e0d
MartinaSulova <64073790+MartinaSulova@users.noreply.github.com>
2020-06-23 15:50:00 +0200

    kk

--- src/views/Servis.vue
+++ src/views/Servis.vue
@@ -5,9 +5,11 @@
 
       <h1>SERVIS</h1>
         <h3>  Záruční a pozáruční servis a prodej náhradních dílů pro stroje XCMG v České Republice zajišťuje: </h3>
-          ​<p>PKBAU s.r.o. – Světlá nad Sázavou – prodej strojů, příslušenství a náhradních dílů, servis, půjčovna, lokality: Beroun, České Budějovice, Liberec, Bruntál, Lovosice
-          Rocktech s.r.o. – Plzeň – prodej strojů a příslušenství, servis strojů a příslušenství
-          VZ service - Vladimír Zelenka - České Budějovice - prodej strojů a příslušenství, servis strojů a tiltrotátorů engcon</p>
+            <p>PKBAU s.r.o. – Světlá nad Sázavou – prodej strojů, příslušenství a náhradních dílů, servis, půjčovna, 
+              lokality: Beroun, České Budějovice, Liberec, Bruntál, Lovosice
+              Rocktech s.r.o. – Plzeň – prodej strojů a příslušenství, servis strojů a příslušenství
+              VZ service - Vladimír Zelenka - České Budějovice - prodej strojů a příslušenství, 
+              servis strojů a tiltrotátorů engcon</p>
         <h3>Pro servis strojů SANY, ale i CAT, Volvo, JCB, Bobcat, Komatsu volejte prosím: </h3>
 
           <p>Telefon: +420 733 798 032
@@ -23,7 +25,7 @@
           <br/>E-Mail: dily@xcmg.cz   </p>
     </div>
     <div class="servisObrazek">
-      <img src="bagr.jpg" alt="Bagr">
+      <img class="imgServis" src="bagr.jpg" alt="Bagr">
 
     </div>
     
@@ -46,8 +48,7 @@ export default {
 
 .servisText,
 .servisObrazek{
-  width: 10%;
-  flex-basis: 50%;
+    flex-basis: 50%;
    margin-left: 30px;
   
 }
@@ -60,6 +61,16 @@ export default {
 .servis p{
   margin-bottom: 10px;
   text-align: justify;
+  line-height: 1.5;
+}
+
+.servisObrazek{
+  margin-top: 40px;
+  margin-right: 30px;
+}
+
+.imgServis{
+  width: 100%;
 }
 
 
