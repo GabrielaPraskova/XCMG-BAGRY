1a106f49028f53431401fcc07730abee14f1d5f1
MartinaSulova <64073790+MartinaSulova@users.noreply.github.com>
2020-06-23 12:00:08 +0200

    servis

--- src/views/Servis.vue
+++ src/views/Servis.vue
@@ -1,27 +1,32 @@
 <template>
-  <div id="servis">
-     
+  <div class="servis">
+    <div class="servisText">
   
 
-    <h1>SERVIS</h1>
-    <h3>  Záruční a pozáruční servis a prodej náhradních dílů pro stroje SANY v České Republice zajišťuje: </h3>
-    ​<p>CARACAL MACHINES s. r. o. – Králův Dvůr u Berouna – prodej strojů, příslušenství a náhradních dílů, servis, půjčovna, lokality: Beroun, České Budějovice, Liberec, Bruntál, Lovosice
-    Rocktech s.r.o. – Králův Dvůr u Berouna – prodej strojů a příslušenství, servis strojů a příslušenství
-    VZ service - Vladimír Zelenka - České Budějovice - prodej strojů a příslušenství, servis strojů a tiltrotátorů engcon</p>
-    <h3>Pro servis strojů SANY, ale i CAT, Volvo, JCB, Bobcat, Komatsu volejte prosím: </h3>
+      <h1>SERVIS</h1>
+        <h3>  Záruční a pozáruční servis a prodej náhradních dílů pro stroje SANY v České Republice zajišťuje: </h3>
+          ​<p>CARACAL MACHINES s. r. o. – Králův Dvůr u Berouna – prodej strojů, příslušenství a náhradních dílů, servis, půjčovna, lokality: Beroun, České Budějovice, Liberec, Bruntál, Lovosice
+          Rocktech s.r.o. – Králův Dvůr u Berouna – prodej strojů a příslušenství, servis strojů a příslušenství
+          VZ service - Vladimír Zelenka - České Budějovice - prodej strojů a příslušenství, servis strojů a tiltrotátorů engcon</p>
+        <h3>Pro servis strojů SANY, ale i CAT, Volvo, JCB, Bobcat, Komatsu volejte prosím: </h3>
 
-    <p>Telefon: +420 604 489 121
+          <p>Telefon: +420 604 489 121
 
-     <br/>E-Mail: servis@caracal-machines.cz</p>
+      <br/>E-Mail: servis@caracal-machines.cz</p>
 
+      
+
+        <h3>Náhradní díly pro stroje SANY, CAT, Volvo, Komatsu, Bobcat, JCB objednávejte:</h3>
+
+          <p>Telefon: +420 604 446 813 
+
+          <br/>E-Mail: dily@caracal-machines.cz   </p>
+    </div>
+    <div class="servisObrazek">
+      <img src="bagr.jpg" alt="Bagr">
+
+    </div>
     
-
-    <h3>Náhradní díly pro stroje SANY, CAT, Volvo, Komatsu, Bobcat, JCB objednávejte:</h3>
-
-    <p>Telefon: +420 604 446 813 
-
-    <br/>E-Mail: dily@caracal-machines.cz   </p>
-    <p></p>
 
   </div>
 </template>
@@ -33,11 +38,28 @@ export default {
 </script>
 
 <style>
-#servis p{
-  text-align:left;
-  margin-left: 10px;
-  padding-left: 10px;
+.servis{
+  display: flex;
+  margin-top: 30px   
+  ; 
+}
+
+.servisText,
+.servisObrazek{
+  width: 10%;
+  flex-basis: 50%;
+   margin-left: 30px;
   
+}
+
+.servisText h1, h3 {
+    color: #3498db;
+    margin-bottom: 10px;
+}
+
+.servis p{
+  margin-bottom: 10px;
+  text-align: justify;
 }
 
 
