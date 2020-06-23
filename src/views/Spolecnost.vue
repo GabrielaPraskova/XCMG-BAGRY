0b46d8c84e3dea6fed1684d388aaf660a8f82c1e
MartinaSulova <64073790+MartinaSulova@users.noreply.github.com>
2020-06-23 10:32:54 +0200

    o společnosti

--- src/views/Spolecnost.vue
+++ src/views/Spolecnost.vue
@@ -1,14 +1,14 @@
 <template>
-  <div id="oSpolecnosti">
+  <div class="oSpolecnosti">
     <div class="kontejner">
       <div class="obrazek">
         <img src="building.png" alt="budova" />
       </div>
       <div class="predstaveni">
-        <h3>O nás</h3>
-        <p>Xuzhou Construction Machinery Group Co., Ltd. (XCMG) byla založena v roce 1943. Od té doby XCMG stála v čele čínského průmyslu stavebních strojů a vyvinula se v jednu z největších, nejvlivnějších a nejkonkurenceschopnějších skupin.</p>
+        <h2>O nás</h2>
+        <p class="odstavec">Xuzhou Construction Machinery Group Co., Ltd. (XCMG) byla založena v roce 1943. Od té doby XCMG stála v čele čínského průmyslu stavebních strojů a vyvinula se v jednu z největších, nejvlivnějších a nejkonkurenceschopnějších skupin.</p>
 
-        <p>
+        <p class="odstavec">
           XCMG je
           <strong><span style="font-size: 18px;">5</span></strong>. největší stavební strojní společnost na světě. Je na
           <strong><span style="font-size: 18px;">65</span></strong>. místě v seznamu
@@ -16,7 +16,7 @@
           <strong><span style="font-size: 18px;">44</span></strong>. místě v seznamu 100 nejlepších čínských výrobních podniků a 4. na seznamu 100 nejlepších čínských výrobců strojů.
         </p>
 
-        <p>XCMG pro váš úspěch!</p>
+        <p class="xcmguspech"> <strong><span style="font-size: 25px;"> XCMG pro váš úspěch!</span></strong></p>
       </div>
     </div>
   </div>
@@ -28,11 +28,31 @@ export default {};
 
 
 <style>
+.kontejner{
+  display: flex;
+  justify-content: space-around;
+  margin-top: 30px;
+  line-height: 1.5;
+}
 .predstaveni {
-  border: 1px solid grey;
+  
   width: 700px;
   padding: 20px;
   text-align: left;
+}
+
+.predstaveni h2{
+  color: #3498db;
+  margin-bottom: 10px;
+}
+
+.odstavec{
+  padding-bottom: 10px;
+}
+
+.xcmguspech{
+  color: #3498db;
+  margin-top: 10px;
 }
 
 
